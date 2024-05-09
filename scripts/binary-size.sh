b6c076ba8ce9e283c66e190349672532411a50d7
Jérémie Drouet <jeremie.drouet@gmail.com>
2024-05-09 11:34:31 +0200

    ci: update script
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

---
+++ scripts/binary-size.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -xe
+
+cargo build --release
+
+export RUNNER_ARCH=$(uname -m)
+export RUNNER_OS=$(uname)
+export RUSTC_VERSION=$(rustc --version)
+export BINARY_SIZE=$(ls -la target/release/git-metrics | cut -d ' ' -f 5)
+
+target/release/git-metrics add binary-size \
+    --tag "platform.arch: $RUNNER_ARCH" \
+    --tag "platform.os: $RUNNER_OS" \
+    --tag "rust.version: $RUSTC_VERSION" \
+    $BINARY_SIZE
