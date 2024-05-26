c58b4c66c0d617b620d1280e25d6af0286aaf581
Jérémie Drouet <jeremie.drouet@gmail.com>
2024-05-26 09:39:47 +0200

    feat: create log command
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

---
+++ src/cmd/log.rs
@@ -0,0 +1,36 @@
+use crate::repository::Repository;
+use std::io::Write;
+
+/// Add a metric related to the target
+#[derive(clap::Parser, Debug, Default)]
+pub(crate) struct CommandLog {
+    /// Commit range, default to HEAD
+    ///
+    /// Can use ranges like HEAD~2..HEAD
+    #[clap(default_value = "HEAD")]
+    target: String,
+}
+
+impl super::Executor for CommandLog {
+    fn execute<Repo: Repository, Out: Write, Err: Write>(
+        self,
+        _repo: Repo,
+        _stdout: &mut Out,
+        _stderr: &mut Err,
+    ) -> Result<(), super::Error> {
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use clap::Parser;
+
+    use super::CommandLog;
+
+    #[test]
+    fn should_parse_range() {
+        let cmd = CommandLog::parse_from(["_", "HEAD~4..HEAD"]);
+        assert_eq!(cmd.target, "HEAD~4..HEAD");
+    }
+}
